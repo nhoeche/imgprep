c14edfe058253797ac90493219e9d8e7eba88692
cripcate <n.hoeche@googlemail.com>
2017-01-31 17:49:15 +0100

    Added module imports, a docstring and TODOs for parts of the script

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import os
+import glob
 from PIL import Image
+import numpy as np
+
+
+'''
+This is the [imgprep] image-preparation-script. It should read in triplets of
+images. Then it should recognize a red square in each, denoting the object of
+interest. It can then crop to the contents of the square, put the images next
+to each other and insert custom scalebars based on magnification.
+'''
 
 
 def main():
@@ -7,12 +18,23 @@ def main():
     pathes = []
     load_images(filenames, pathes)
 
+
 def load_images(filenames, pathes):
 
-    img = list(length(filenames))
+    img = list(len(filenames))
 
     for i, (filename, path) in enumerate(zip(filenames, pathes)):
         img[i] = Image.open('{}/{}'.format(filename, path))
+
+        # TODO: Save metadata and image objects into numpy array / pandas
+        #       frame
+        # TODO: Rotate the second polarized image by -45°
+
+
+# TODO: Recognize the Magnification and calculate scale-bar dimensions
+# TODO: Recognize the Red square coordinates
+# TODO: Crop the images and place them next to each other
+# TODO: Create three scale-bars and insert them into the images
 
 
 if __name__ == "__main__":
