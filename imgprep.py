3666ea267ad5ebfc36b3aeb29b2544884b18fdb4
cripcate <n.hoeche@googlemail.com>
2017-02-22 22:52:51 +0100

    argparse transition in progress. Added a shebang and a basic argparse menu. Furhter testing commencing

--- imgprep.py
+++ imgprep.py
@@ -1,5 +1,4 @@
-import sample
-
+#!/usr/bin/env python3
 '''
 This is the [imgprep] image-preparation-script. It should read in triplets of
 images. Then it should recognize a red square in each, denoting the object of
@@ -7,18 +6,38 @@ interest. It can then crop to the contents of the square, put the images next
 to each other and insert custom scalebars based on magnification.
 '''
 
+import argparse
+import sample
+
 
 def main():
+    # Create the argparser
+    parser = argparse.ArgumentParser(usage=__doc__)
 
-    names = ['Hofer2_63x_gekreuzt_evtl-coccolithB2.jpg',
-             'Hofer2_63x_gekreuzt_evtl-coccolithB2.jpg']
-    pathes = ['sample_images', 'sample_images']
+    # Sample info
+    parser.add_argument('sample', help='The name of the sample to be treated.')
 
-    specimen = sample.Sample(filenames=names,  pathes=pathes)
-    # Should write a "get filenames" function for this, possibly with argparse
+    # Image loading
+    parser.add_argument('images', help='Filenames of images associated to the '\
+                        'sample.')
 
-    specimen.load_images()
+    # Verbosity
+    parser.add_argument('-v', '--verbose', help='Verbosity modus. Prints more '\
+                        'info on the screen.', action='store_true')
+
+    args = parser.parse_args()
+
+#   ----------------------------------------------------------------------------
+
+    # Generating a specimen
+    specimen = sample.Sample(sample_name=args.sample)
+
+    # Loading image pathes
+    if args.images:
+        specimen.image_pathes = [path for path in args.images]
+        specimen.load_images()
 
 
 if __name__ == "__main__":
+
     main()
