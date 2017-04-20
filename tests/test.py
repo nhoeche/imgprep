d57862d73e069153501872a0f31709b1ac2a9f83
cripcate <n.hoeche@googlemail.com>
2017-04-20 22:46:08 +0200

    test structure rewrite. LoadImageTest still fails

--- tests/test.py
+++ tests/test.py
@@ -18,42 +18,49 @@ class ArgParserTest(unittest.TestCase):
 
 
 # -- sample.py
-# -- -- Sample
-class SampleTest(unittest.TestCase):
+# __init__ methods
+class InitTest(unittest.TestCase):
     def setUp(self):
         self.sample = sample.Sample('test_sample')
-
-    def test_init(self):
-        self.assertEqual('test_sample', self.sample.sample_name)
-        self.assertEqual([], self.sample.image_list)
-        self.assertEqual(0, self.sample.image_count)
-        self.assertEqual([], self.sample.cropped_images)
-
-    def test_loading(self):
-        self.sample.load_images(['tests/test.png'])
-
-        self.assertEqual(1, self.sample.image_count)
-
-        self.assertItemsEqual(sample.Image('tests/test.png'),
-                              self.sample.image_list[0])
-
-
-# -- -- Image
-class ImageTest(unittest.TestCase):
-    def setUp(self):
         self.img = sample.Image('tests/test.png')
 
-    def test_init(self):
+    def sample_init_test(self):
+        self.assertEqual('test_sample', self.sample.sample_name)
+        self.assertEqual(0, self.sample.image_count)
+        self.assertEqual([], self.sample.image_list)
+        self.assertEqual([], self.sample.cropped_images)
+
+    def image_init_test(self):
         # Name
         self.assertEqual('tests/test.png', self.img.path)
         self.assertEqual('test', self.img.name)
         self.assertEqual('.png', self.img.extension)
-        # Image
+        # Image-data
         np.testing.assert_array_equal(io.imread('tests/test.png'), self.img.image)
         # ROI parameters
         self.assertEqual([], self.img.box_dim)
         self.assertEqual([], self.img.box_coords)
 
+
+class LoadImageTest(unittest.TestCase):
+    def setUp(self):
+        self.sample = sample.Sample('test_sample')
+
+    def test_single_image(self):
+        # Loading
+        self.sample.load_images(['tests/test.png'])
+        # Testing
+        self.assertEqual(1, self.sample.image_count)
+        self.assertItemsEqual(sample.Image('tests/test.png'),
+                              self.sample.image_list[0])
+
+    def test_multiple_images(self):
+        # Loading
+        self.sample.load_images(['tests/test.png', 'tests/test1.png'])
+        # Testing
+        self.assertEqual(2, self.sample.image_count)
+        self.assertItemsEqual(sample.Image('tests/test.png'),
+                              self.sample.image_list[0])
 
 if __name__ == "__main__":
     unittest.main()
